b6d2967003353caecd2f01464f232b5b422048a1
Alexandry Cornita <alexandry.cornita.spo16@ecusolna.onmicrosoft.com>
2018-04-24 14:02:45 +0200

    Added "Link" column and coded the JS Video player

--- LIA.Data/Migrations/20180424110852_AddItem.Designer.cs
+++ LIA.Data/Migrations/20180424110852_AddItem.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace LIA.Data.Migrations
 {
     [DbContext(typeof(VideoContext))]
-    partial class VideoContextModelSnapshot : ModelSnapshot
+    [Migration("20180424110852_AddItem")]
+    partial class AddItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LIA.Data/Migrations/20180424110852_AddItem.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LIA.Data.Migrations
+{
+    public partial class AddItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+            migrationBuilder.AddColumn<string>(
+                name: "Link",
+                table: "Items",
+                nullable: true);
+
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.DropColumn(
+                name: "Link",
+                table: "Items");
+
+       
+        }
+    }
+}
