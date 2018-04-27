eeab3aef098e4db393381066ce5313d22612a4a0
Alexandry Cornita <alexandry.cornita.spo16@ecusolna.onmicrosoft.com>
2018-04-27 15:41:51 +0200

    Almost done with the admin interface

--- LIA.Admin/Pages/Index.cshtml.cs
+++ LIA.Admin/Pages/Index.cshtml.cs
@@ -12,21 +12,42 @@ namespace LIA.Admin.Pages
 {
 	[Authorize(Roles = "Admin")]
 	public class IndexModel : PageModel
-    {
-        //public string text;
-        IDbReader _db;
-		public List<ItemType> slask;
+	{
+		//public string text;
+		IDbReader _db;
+		public List<Item> items;
+		public List<ItemType> itemtypes;
+		public List<ListItem> listitems;
+		public List<Product> products;
+		public List<UserProduct> userproducts;
 
+		//public int item;
+		//public int product;
 
 		public IndexModel(IDbReader db)
-        {
-            //text = "Hello WOrld";
-            _db = db;
-        }
+		{
+			//text = "Hello WOrld";
+			_db = db;
+		}
 
-        public void OnGet()
-        {
-			slask  = _db.Get<ItemType>().ToList();
-        }
-    }
+		//public void ItemCount()
+		//{
+		//	item = _db.Get<Item>().ToList().Count();
+		//}
+
+		//public void ProductCount()
+		//{
+		//	product = _db.Get<Product>().ToList().Count();
+		//}
+
+		(int itemcount, int itemtypecount, int listitemcount, int productcount, int userproductcount) Count()
+			{
+			return (itemcount: _db.Get<Product>().Count(),
+				itemtypecount: _db.Get<ItemType>().Count(),
+				listitemcount: _db.Get<ListItem>().Count(),
+				productcount: _db.Get<Product>().Count(),
+				userproductcount: _db.Get<UserProduct>().Count());
+			}
+
+	}
 }
