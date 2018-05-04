9dc702e1c30de4e5b7cac076103eadf47f49630f
Alexandry Cornita <alexandry.cornita.spo16@ecusolna.onmicrosoft.com>
2018-05-04 11:41:50 +0200

    Minor changes left

--- LIA.Data/Data/Entities/ListItem.cs
+++ LIA.Data/Data/Entities/ListItem.cs
@@ -11,7 +11,7 @@ namespace LIA2Version3.Data.Entities
 		[Key]
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public int ItemId { get; set; }
+		//public int ItemId { get; set; }
 		public string Description { get; set; }
 		public int ProductId{ get; set; }
 
---
+++ LIA.Data/Migrations/20180504084020_removed-itemid.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LIA.Data.Migrations
+{
+    public partial class removeditemid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "ListItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ListItemId",
+                table: "Items",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItemId",
+                table: "ListItems",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ListItemId",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(int));
+		}
+    }
+}
