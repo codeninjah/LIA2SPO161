a559c93d87ba858b66c56cf8c168ecdfb6117903
Alexandry Cornita <alexandry.cornita.spo16@ecusolna.onmicrosoft.com>
2018-05-08 11:02:53 +0200

    Changes to the UI

--- LIA.Data/Migrations/20180508081458_addedPictureToAuthorTable.Designer.cs
+++ LIA.Data/Migrations/20180508081458_addedPictureToAuthorTable.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace LIA.Data.Migrations
 {
     [DbContext(typeof(VideoContext))]
-    partial class VideoContextModelSnapshot : ModelSnapshot
+    [Migration("20180508081458_addedPictureToAuthorTable")]
+    partial class addedPictureToAuthorTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LIA.Data/Migrations/20180508081458_addedPictureToAuthorTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LIA.Data.Migrations
+{
+    public partial class addedPictureToAuthorTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "Authors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "Authors");
+        }
+    }
+}
